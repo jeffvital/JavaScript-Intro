a5c2a319900b25c408c680e97d31fa12fb05c7b5
jeffvital <Jeffersonjvs@outlook.com>
2023-01-25 14:51:34 -0300

    challenge with const, typeof, let solved

--- Start/challenge_area.js
+++ Start/challenge_area.js
@@ -1,2 +1,9 @@
 // Calculo da area
 //PI * raio * raio
+
+const PI = 3.141592;
+let raio = 4;
+let result = PI * raio * raio;
+console.log("valor de pi: " + PI + ", valor do raio: " + raio);
+console.log("O raio da circunferencia é: " + result + "!");
+console.log("Do tipo: " + typeof PI)
