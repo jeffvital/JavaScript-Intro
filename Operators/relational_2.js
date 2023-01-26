b2b5a33aea779f93f7290cc453f6d0ae9fce2592
jeffvital <Jeffersonjvs@outlook.com>
2023-01-26 13:50:53 -0300

    relational and types !important

---
+++ Operators/relational_2.js
@@ -0,0 +1,18 @@
+let a = 1;
+let b = "1";
+let c = "1";
+
+// Igualdade!
+console.log(a == b);
+
+// Aqui importa não apenas se é igual mas do mesmo tipo!
+console.log(a === b);
+console.log(b === c);
+console.log("------------------");
+
+// Diferença!
+console.log(a != b);
+
+// Aqui importa não apenas se é igual mas do mesmo tipo!
+console.log(a !== b);
+console.log(b !== c);
