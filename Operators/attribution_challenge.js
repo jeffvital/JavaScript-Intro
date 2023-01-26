97b74d09320f691c46639946a380928511dd3486
jeffvital <Jeffersonjvs@outlook.com>
2023-01-26 14:53:27 -0300

    attibution with text and \n line

---
+++ Operators/attribution_challenge.js
@@ -0,0 +1,18 @@
+/**
+ * Lista de Aprovados
+ * ===================
+ *
+ * 1. Ana Silva
+ * 2. Pedro Albuquerque
+ * 3. Guilherme Pereira
+ * 4. Rebeca França
+ */
+
+let text =
+  "Lista de Aprovados\n" +
+  "=================\n\n" +
+  "1. Ana Silva\n" +
+  "2. Pedro Albuquerque\n" +
+  "3. Guilherme Pereira\n" +
+  "4. Rebeca França";
+console.log(text);
