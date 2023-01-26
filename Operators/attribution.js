b3bbd1febe5e0a064fc82d9632d28d9f31effc6a
jeffvital <Jeffersonjvs@outlook.com>
2023-01-26 14:38:11 -0300

    attribution

---
+++ Operators/attribution.js
@@ -0,0 +1,17 @@
+let x = 10;
+x = x + 6;
+x = x - 1;
+
+let y = 20;
+y += 10;
+y -= 2;
+y *= 2;
+y /= 4;
+
+console.log(x);
+console.log(y);
+
+let w = "text";
+w += ", more text";
+
+console.log(w);
