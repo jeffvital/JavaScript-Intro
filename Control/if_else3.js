03c52f6ec0fe3fbd5124346aecda48e39224ead8
jeffvital <Jeffersonjvs@outlook.com>
2023-02-03 17:10:41 -0300

    if - else if

--- Control/if_else3.js
+++ Control/if_else3.js
@@ -1,14 +1,14 @@
-const hora = 17;
+const hora = 16;
 let saudacao;
 
 if (hora < 12) {
   saudacao = "Bom dia!";
+} else if (hora < 18) {
+  saudacao = "Boa tarde!";
+} else if (hora < 22) {
+  saudacao = "Boa noite!";
 } else {
-  if (hora < 18) {
-    saudacao = "Boa tarde!";
-  } else {
-    saudacao = "Boa noite!";
-  }
+  saudacao = "Muito tarde!";
 }
 
 console.log(saudacao);
