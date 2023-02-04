95d4427bedf0b707b376e65f686a767e93de7b13
jeffvital <Jeffersonjvs@outlook.com>
2023-02-04 11:20:51 -0300

    if_else_challenge

---
+++ Control/if_else_challenge.js
@@ -0,0 +1,18 @@
+const a = 10;
+const b = 28;
+const operacao = "%"; // + - * / %
+
+let result = 2;
+if (operacao == "+") {
+  result = a * b;
+} else if (operacao == "-") {
+  result = a - b;
+} else if (operacao == "*") {
+  result = a * b;
+} else if (operacao == "/") {
+  result = a / b;
+} else if (operacao == "%") {
+  result = a % b;
+}
+
+console.log(result);
