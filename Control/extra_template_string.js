31f194b7ea47886ea634a7049ee001882ab5cf72
jeffvital <Jeffersonjvs@outlook.com>
2023-02-07 17:15:30 -0300

    usando crase ao inves de aspas

---
+++ Control/extra_template_string.js
@@ -0,0 +1,12 @@
+const i = 2;
+const j = 4;
+
+// USANDO ASPAS DUPLAS.
+console.log("i = " + i + ", j = " + j);
+
+// USANDO ASPAS SIMPLES MESMO RESULTADO COM DUPLAS.
+console.log("i = " + i + ", j = " + j);
+
+/// USANDO CRASE IDEIA SIMILAR AS ASPAS MAS PODENDO ESCREVER EXPRESÕES DENTRO DA CRASE USANDO O SIFRÃO $.
+console.log(`i = ${i + j}, j = ${j}`);
+console.log(`1 + 2 = ${1 + 2}`);
