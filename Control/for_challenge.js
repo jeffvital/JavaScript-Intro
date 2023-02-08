607436df7d1ef97f144b470f134e593e36203427
jeffvital <Jeffersonjvs@outlook.com>
2023-02-08 17:08:38 -0300

    challenger answer

---
+++ Control/for_challenge.js
@@ -0,0 +1,19 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// let linha = "";
+
+// for (let i = 1; i <= 6; i++) {
+//   linha += "#";
+//   console.log(linha);
+// }
+
+// SEM NÚMEROS!!
+
+for (let i = "#"; i <= "######"; i += "#") {
+  console.log(i);
+}
