6b01c8d20a18a92c4e8f7faaf037d8e15a72e0af
jeffvital <Jeffersonjvs@outlook.com>
2023-02-09 15:47:49 -0300

    with_functions

---
+++ functions/with_function.js
@@ -0,0 +1,23 @@
+const d1 = 3;
+const m1 = 7;
+const a1 = 2021;
+
+function showData1() {
+  console.log(`${d1}/${m1}/${a1}`);
+}
+
+const d2 = 23;
+const m2 = 12;
+const a2 = 2022;
+
+function showData2() {
+  console.log(`${d2}-${m2}-${a2}`);
+}
+
+showData1();
+showData1();
+showData1();
+
+showData2();
+showData2();
+showData2();
