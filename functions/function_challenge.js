6e046ad20656f647e7c9efa2a757d1bb70e1d1b5
jeffvital <Jeffersonjvs@outlook.com>
2023-02-10 17:09:14 -0300

    answer for the challenge

---
+++ functions/function_challenge.js
@@ -0,0 +1,13 @@
+function soma(som1, som2) {
+  console.log(som1 + som2);
+}
+
+soma(3, 4);
+soma(31, 98);
+
+function subtracao(sub1, sub2) {
+  console.log(sub1 - sub2);
+}
+
+subtracao(11, 7);
+subtracao(19, 52);
