f57a940dfb4a780241fc04890431e59c6825719f
jeffvital <Jeffersonjvs@outlook.com>
2023-02-14 09:54:14 -0300

    teacher answer

--- functions/challenge_functios_2.js
+++ functions/challenge_functios_2.js
@@ -1,41 +1,75 @@
-function conceito(nota) {
-  switch (nota) {
+// function conceito(nota) {
+//   switch (nota) {
+//     case 10:
+//       console.log("A+");
+//       break;
+//     case 9:
+//       console.log("A");
+//       break;
+//     case 8:
+//       console.log("B+");
+//       break;
+//     case 7:
+//       console.log("B");
+//       break;
+//     case 6:
+//       console.log("C+");
+//       break;
+//     case 5:
+//       console.log("C");
+//       break;
+//     case 4:
+//       console.log("D+");
+//       break;
+//     case 3:
+//       console.log("D");
+//       break;
+//     case 2:
+//       console.log("E+");
+//       break;
+//     case 1:
+//       console.log("E");
+//       break;
+//     case 0:
+//       console.log("F");
+//       break;
+//     default:
+//       console.log("Nulo!");
+//   }
+// }
+
+// let result = conceito(8);
+
+// FORM COMO O PROFESSOR FEZ USANDO RETURN
+
+function notaParaConceito(nota) {
+  switch (Math.ceil(nota)) {
     case 10:
-      console.log("A+");
-      break;
+      return "A+";
     case 9:
-      console.log("A");
-      break;
+      return "A";
     case 8:
-      console.log("B+");
-      break;
+      return "B+";
     case 7:
-      console.log("B");
-      break;
+      return "B";
     case 6:
-      console.log("C+");
-      break;
+      return "C+";
     case 5:
-      console.log("C");
-      break;
+      return "C";
     case 4:
-      console.log("D+");
-      break;
+      return "D+";
     case 3:
-      console.log("D");
-      break;
+      return "D";
     case 2:
-      console.log("E+");
-      break;
+      return "E+";
     case 1:
-      console.log("E");
-      break;
+      return "E";
     case 0:
-      console.log("F");
-      break;
+      return "F";
     default:
-      console.log("Nulo!");
+      return null;
   }
 }
 
-let result = conceito(8);
+console.log(`O conceito do Bruno e ${notaParaConceito(6)}`);
+console.log(`O conceito da Lauro é ${notaParaConceito(8.9)}`);
