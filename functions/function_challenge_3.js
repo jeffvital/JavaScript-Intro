467de0bc3ce4caebe906a0c31abc3ff878822f6e
jeffvital <Jeffersonjvs@outlook.com>
2023-02-15 16:52:02 -0300

    status do aluno

--- functions/function_challenge_3.js
+++ functions/function_challenge_3.js
@@ -41,11 +41,38 @@
 // RESOLUÇÃO DO PROFESSOR
 
 const nota1 = 7.8;
-const nota2 = 8.1;
-const nota3 = 2.7;
+const nota2 = 3.1;
+const nota3 = 2;
 
+// AQUI DESCOBRIMOS A MENOR NOTA DAS TRÊS FORNECIDAS.
 function minimo(n1, n2) {
   return n1 <= n2 ? n1 : n2;
 }
 
-console.log(minimo(nota1, minimo(nota2, nota3)))
+// AQUI FAZEMOS A MÉDIA EXCLUINDO A MENOR DAS TRÊS NOTAS FORNECIDAS.
+function mediaDuasMaiores(n1, n2, n3) {
+  const menorNota = minimo(n1, minimo(n2, n3));
+
+  if (menorNota === n1) {
+    return (n2 + n3) / 2;
+  } else if (menorNota === n2) {
+    return (n1 + n3) / 2;
+  } else {
+    return (n1 + n2) / 2;
+  }
+}
+
+// STATUS DA MEDIA
+function mediaParaStatus(media) {
+  if (media >= 7) {
+    return "Aprovado";
+  } else if (media >= 4) {
+    return "Recuperação";
+  } else {
+    return "Reprovado";
+  }
+}
+
+const mediaFinal = mediaDuasMaiores(nota1, nota2, nota3);
+const statusFinal = mediaParaStatus(mediaFinal);
+console.log(`O resultado final do aluno é: ${statusFinal}`);
