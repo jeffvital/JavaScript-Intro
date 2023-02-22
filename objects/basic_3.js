e395fa1fc699c6dabd80555c1640ce75de78b2a7
jeffvital <Jeffersonjvs@outlook.com>
2023-02-22 16:43:11 -0300

    baisc 3

---
+++ objects/basic_3.js
@@ -0,0 +1,11 @@
+const data = {
+  dia: 1,
+  mes: 10,
+  ano: 2023,
+};
+
+// DESSA FORMA É POSSÍVEL ALTERAR OS VALORES DO OBJETO
+data.dia = 24;
+data.mes = 12;
+
+console.log(`${data.dia}/${data.mes}/${data.ano}`);
