2427bc888b573aeb26daa8f7e05171fdc3a96516
jeffvital <Jeffersonjvs@outlook.com>
2023-02-22 16:01:50 -0300

    basic 1

---
+++ objects/basic_1.js
@@ -0,0 +1,21 @@
+const numeros = [10, 20, 30]; // ISSO É UM ARRAY
+
+// ISSO É UM OBJETO
+const produto = {
+  nome: "Caneta",
+  preco: 9.99,
+  desconto: 0.2,
+};
+
+console.log(numeros[0]);
+console.log(produto["nome"]);
+console.log(produto["preco"]);
+console.log(produto["desconto"]);
+
+// PODE SER ACESSADO APENAS COM UM PONTO
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.desconto);
+
+console.log(typeof produto);
