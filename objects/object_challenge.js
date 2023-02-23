38c5748df0b0cd287c389c1a9d7b7088b088df74
jeffvital <Jeffersonjvs@outlook.com>
2023-02-23 16:45:06 -0300

    correction

--- objects/object_challenge.js
+++ objects/object_challenge.js
@@ -3,8 +3,8 @@ const data = {
   mes: "4",
   ano: "1994",
   exibir: function () {
-    return console.log(`${this.dia}/${this.mes}/${this.ano}`);
+    return `${this.dia}/${this.mes}/${this.ano}`;
   },
 };
 
-data.exibir();
+console.log(data.exibir());
